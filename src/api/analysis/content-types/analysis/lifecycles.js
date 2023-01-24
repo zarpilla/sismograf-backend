a33286bb73853dcb47d486a54075beaa1eacdb30
Jordi Sabaté <jordi@webcoop.cat>
2023-01-24 21:29:10 +0100

    application & texts entities

--- src/api/analysis/content-types/analysis/lifecycles.js
+++ src/api/analysis/content-types/analysis/lifecycles.js
@@ -1,73 +1,80 @@
-var _ = require('lodash');
-
+var _ = require("lodash");
 
 const calculateResilienceLevel = async (data) => {
+  if (data._internal) {
+    return;
+  }
 
-    // console.log('calculateResilienceLevel data', data)
-    if (data._internal) {
-        return
-    }        
-
-
-    try {
-        await strapi.query('api::analysis.analysis').update({ where: { id: data.id }, data: { _internal: true } })
-    }
-    catch (e) {
-        console.error(e)
-        await strapi.query('api::analysis.analysis').update({ where: { id: data.id }, data: { resilienceLevel: 0, _internal: true } })
-    }
-    
-}
-
+  try {
+    await strapi
+      .query("api::analysis.analysis")
+      .update({ where: { id: data.id }, data: { _internal: false } });
+  } catch (e) {
+    console.error(e);
+    await strapi.query("api::analysis.analysis").update({
+      where: { id: data.id },
+      data: { resilienceLevel: 0, _internal: false },
+    });
+  }
+};
 
 module.exports = {
-      
-    async afterCreate(event) {
-        await calculateResilienceLevel(event.result)
-    },
+  async afterCreate(event) {
+    await calculateResilienceLevel(event.result);
+  },
 
-    async beforeUpdate(event) {
-        
-        const id = event?.params?.where?.id
+  async afterUpdate(event) {
+    const id = event?.params?.where?.id;
 
-        if (!id) {
-            return
+    if (!id) {
+      return;
+    }
+    if (event.params.data._internal) {
+      return;
+    }
+    try {
+      const analysis = await strapi
+        .service("api::analysis.analysis")
+        .findOne(id, {
+          populate: [
+            "results",
+            "results.indicator",
+            "results.indicator.pattern",
+            "results.indicator.pattern.principle",
+            "results.indicator.pattern.principle.domain",
+          ],
+        });
+
+      analysis.results.forEach((r) => {
+        if (r.indicator) {
+          r.analysisId = analysis.uid;
+          // r.templateId = analysis.template?.id;
+          // r.templateName = analysis.template?.name;
+          // r.questionnaireId = analysis.questionnaire?.id;
+          // r.questionnaireName = analysis.questionnaire?.name;
+          r.domainId = r.indicator.pattern.principle.domain.id;
+          r.domainName = r.indicator.pattern.principle.domain.name;
+          r.domainDescription =
+            r.indicator.pattern.principle.domain.description;
+          r.principleId = r.indicator.pattern.principle.id;
+          r.principleName = r.indicator.pattern.principle.name;
+          r.patternId = r.indicator.pattern.id;
+          r.patternName = r.indicator.pattern.name;
+          // r.pattern = r.indicator.pattern
+          r.indicatorId = r.indicator.id;
+          r.indicatorName = r.indicator.question;
+          //   r.responseValue = r.indicator.indicator_options.find(
+          //     (o) => o.value === r.value
+          //   ).name;
+          r.comments = analysis.comments?.find(
+            (c) => c.indicator && c.indicator.id === r.indicator.id
+          )?.comment;
+          r.resilienceLevel = r.value;
         }
+        delete r.indicator;
+      });
 
-        const analysis = await strapi.service('api::analysis.analysis').findOne(id, {
-            populate:  ['results', 'results.indicator', 'results.indicator.pattern', 'results.indicator.pattern.principle', 'results.indicator.pattern.principle.domain']
-        });
-
-        analysis.results.forEach((r) => {
-            if (r.indicator) {
-              r.analysisId = analysis.uid;
-              // r.templateId = analysis.template?.id;
-              // r.templateName = analysis.template?.name;
-              // r.questionnaireId = analysis.questionnaire?.id;
-              // r.questionnaireName = analysis.questionnaire?.name;
-              r.domainId = r.indicator.pattern.principle.domain.id;
-              r.domainName = r.indicator.pattern.principle.domain.name;
-              r.domainDescription = r.indicator.pattern.principle.domain.description;
-              r.principleId = r.indicator.pattern.principle.id;
-              r.principleName = r.indicator.pattern.principle.name;
-              r.patternId = r.indicator.pattern.id;
-              r.patternName = r.indicator.pattern.name;
-              // r.pattern = r.indicator.pattern
-              r.indicatorId = r.indicator.id;
-              r.indicatorName = r.indicator.question;
-            //   r.responseValue = r.indicator.indicator_options.find(
-            //     (o) => o.value === r.value
-            //   ).name;
-              r.comments = analysis.comments?.find(
-                (c) => c.indicator && c.indicator.id === r.indicator.id
-              )?.comment;
-              r.resilienceLevel = r.value
-            }
-            delete r.indicator;
-        });
-
-
-        const summaryByDomain = _(analysis.results)
+      const summaryByDomain = _(analysis.results)
         .groupBy("domainId")
         .map((domainRows, id) => ({
           domainDescription: id,
@@ -87,7 +94,21 @@ module.exports = {
             .value(),
         }))
         .value();
-        const resilienceLevel = _.meanBy(summaryByDomain, "resilienceLevel")        
-        event.params.data.resilienceLevel = resilienceLevel
-    }    
+
+      const resilienceLevel = _.meanBy(summaryByDomain, "resilienceLevel");
+
+      const updateObject = {
+        id,
+        resilienceLevel: resilienceLevel,
+        _internal: true,
+      };
+
+      await strapi.query("api::analysis.analysis").update({
+        where: { id: id },
+        data: { resilienceLevel: resilienceLevel, _internal: true },
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  },
 };
